8b15d04941147e6f18e01b7a28791087d91f1ade
adrieldussex <adrieldussex@gmail.com>
2023-03-22 10:45:00 -0300

    About Page

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+export default function About() {
+    return (
+        <div className="bg-red-500 w-full h-screen flex justify-center items-center">
+            <div className="bg-[url('https://static.wixstatic.com/media/f8eba5_460d0bdab99f487188a17916c22a9abe~mv2.jpg/v1/fill/w_952,h_1050,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/f8eba5_460d0bdab99f487188a17916c22a9abe~mv2.jpg')] bg-zinc-300 w-1/2 h-full flex justify-center items-center">
+                <div className="h-2/3 w-1/2 bg-neutral-900"></div>
+            </div>
+            <div className="text-zinc-100 text-start flex flex-col justify-center items-center bg-neutral-900 w-1/2 h-full">
+                <div className="w-1/2 h-2/3">
+                    <h2 className="text-5xl font-semibold mb-4">Hablemos!</h2>
+                    <p className="text-xl font-light">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quo dicta dolorum, molestias facilis doloribus corporis quae ullam aliquid quaerat odit minima cumque omnis voluptatibus distinctio fugit illum rerum culpa nisi incidunt a quam perferendis. Eveniet soluta voluptatum nam dolorum libero cum asperiores nulla, ratione similique ipsam architecto quae voluptatem impedit et consequatur repellendus explicabo rerum esse vitae! Eveniet nisi, optio, quis totam molestiae fuga officiis veniam, maxime culpa asperiores ratione? Doloribus ratione ea eligendi nulla voluptas neque corrupti rerum laborum accusamus beatae, quam aspernatur aliquam sint perferendis eum nostrum dolorem culpa quia eveniet delectus ex numquam molestiae ut? Ipsa quia pariatur est, culpa eveniet alias facere illum excepturi voluptas a eius blanditiis hic recusandae commodi reprehenderit sequi vero enim.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}

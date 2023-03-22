f14aabbfa4b44acf9c1fd331d32854a362d7b29c
adrieldussex <adrieldussex@gmail.com>
2023-03-22 10:45:19 -0300

    Agregué About

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import About from "../components/About";
 import Header from "../components/Header";
 
 export default function Home() {
     return (
-        <div className="w-8/12 mx-auto h-screen flex flex-col justify-center items-center">
-            <h2 className="text-zinc-400 text-lg">DESIGNER & WEB DEVELOPER</h2>
-            <h1 className="text-zinc-100 font-semibold text-5xl">ADRIEL DUSSEX</h1>
-            <Header />
+        <div className="w-full flex flex-col justify-center items-center">
+            <div className="flex flex-col items-center h-screen justify-center w-8/12">                
+                <h2 className="text-zinc-400 text-lg">DESIGNER & WEB DEVELOPER</h2>
+                <h1 className="text-zinc-100 font-semibold text-5xl">ADRIEL DUSSEX</h1>
+                <Header />
+            </div>
+            <About />
         </div>
     )
 }
